acf90f6e43cca8fe38ba4e845b3e96380252c5c6
Nate Finch <finchproservices@gmail.com>
2016-08-24 15:59:46 +0900

    plugin adds custom sidebar and custom widget to wp admin, and also calls the content of the widget to the front end

--- inc/main.php
+++ inc/main.php
@@ -1,2 +1,98 @@
 <?php
 
+class PIDA_Widget extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		$widget_ops = array(
+			'classname'   => 'pida_widget',
+			'description' => 'Place a disclaimer widget into a custom widget area, which will display on all posts or in specific categories.',
+			'customize_selective_refresh' => true,
+		);
+		$control_ops = array( 'width' => 400, 'height' => 350 );
+		parent::__construct( 'my_widget', 'Post Intro Disclaimer Announcements', $widget_ops, $control_ops );
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
+
+		$widget_text = ! empty( $instance['text'] ) ? $instance['text'] : '';
+
+
+
+		/** This filter is documented in wp-includes/widgets/class-wp-widget-pages.php */
+		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
+
+		$widget_text = ! empty( $instance['text'] ) ? $instance['text'] : '';
+
+		/**
+		 * Filters the content of the Text widget.
+		 *
+		 * @since 2.3.0
+		 * @since 4.4.0 Added the `$this` parameter.
+		 *
+		 * @param string         $widget_text The widget content.
+		 * @param array          $instance    Array of settings for the current widget.
+		 * @param WP_Widget_Text $this        Current Text widget instance.
+		 */
+		$text = apply_filters( 'widget_text', $widget_text, $instance, $this );
+
+		echo $args['before_widget'];
+		if ( ! empty( $title ) ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		} ?>
+		<div class="textwidget"><?php echo !empty( $instance['filter'] ) ? wpautop( $text ) : $text; ?></div>
+		<?php
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'text' => '' ) );
+		$filter = isset( $instance['filter'] ) ? $instance['filter'] : 0;
+		$title = sanitize_text_field( $instance['title'] );
+		?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
+
+		<p><label for="<?php echo $this->get_field_id( 'text' ); ?>"><?php _e( 'Content:' ); ?></label>
+			<textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>"><?php echo esc_textarea( $instance['text'] ); ?></textarea></p>
+
+		<p><input id="<?php echo $this->get_field_id('filter'); ?>" name="<?php echo $this->get_field_name('filter'); ?>" type="checkbox"<?php checked( $filter ); ?> />&nbsp;<label for="<?php echo $this->get_field_id('filter'); ?>"><?php _e('Automatically add paragraphs'); ?></label></p>
+		<?php
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = sanitize_text_field( $new_instance['title'] );
+		if ( current_user_can( 'unfiltered_html' ) ) {
+			$instance['text'] = $new_instance['text'];
+		} else {
+			$instance['text'] = wp_kses_post( $new_instance['text'] );
+		}
+		$instance['filter'] = ! empty( $new_instance['filter'] );
+		return $instance;
+	}
+}
+
+add_action( 'widgets_init', function(){
+	register_widget( 'PIDA_Widget' );
+});
--- init.php
+++ init.php
@@ -8,7 +8,7 @@ Author:      Nate Finch
 Author URI:  https://n8finch.com
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: post-intro-disclaimer-announcement-widget
+Text Domain: pida-widget
 */
 
 namespace post_intro_disclaimer_announcement_widget;
@@ -26,4 +26,37 @@ function add_these_plugin_styles_and_scripts() {
 }
 
 
+/**
+ * Register Post Intro Disclaimer Announcements Custom Widget Area
+ */
+
+add_action( 'widgets_init', __NAMESPACE__ . '\add_pida_widget_area_to_widgets' );
+function add_pida_widget_area_to_widgets() {
+
+	register_sidebar( array(
+		'name'          => __( 'Post Intro Disclaimer Announcements', 'pida-widget' ),
+		'id'            => 'pida-widget-area',
+		'description'   => 'Add the Post Intro Disclaimer Announcements Widget to this area (or any widget you would like at the beginning of all posts or specific post categories',
+		'class'         => '',
+		'before_widget' => '<li id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</li>',
+		'before_title'  => '<h2 class="widgettitle">',
+		'after_title'   => '</h2>'
+	) );
+}
+
+
+add_action( 'genesis_before_entry_content', __NAMESPACE__ . '\add_pida_widget_to_post' );
+
+
+function add_pida_widget_to_post() {
+
+	if ( is_active_sidebar( 'pida-widget-area' ) ) :
+		?>
+		<div id="pida-widget-area" class="widget-area" role="complementary">
+			<?php dynamic_sidebar( 'pida-widget-area' );
+			?>
+		</div><!-- #pida-widget-area-sidebar -->
+	<?php endif;
+}
 
