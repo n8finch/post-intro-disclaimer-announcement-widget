158481a9331098661d32bc11654028b5df0ea81b
Nate Finch <finchproservices@gmail.com>
2016-08-24 17:44:44 +0900

    color highlight is all good, need to get text and background colors up

--- inc/main.php
+++ inc/main.php
@@ -25,11 +25,12 @@ class PIDA_Widget extends \WP_Widget {
 	 */
 	public function widget( $args, $instance ) {
 
-		$widget_text = ! empty( $instance['text'] ) ? $instance['text'] : '';
-
 		/** This filter is documented in wp-includes/widgets/class-wp-widget-pages.php */
 
 		$widget_text = ! empty( $instance['text'] ) ? $instance['text'] : '';
+		$pida_highlight = ! empty( $instance['pida_highlight'] ) ? $instance['highlight'] : '';
+		$pida_text = ! empty( $instance['pida_text'] ) ? $instance['pida_text'] : '';
+		$pida_background = ! empty( $instance['pida_background'] ) ? $instance['pida_background'] : '';
 
 		/**
 		 * Filters the content of the Text widget.
@@ -42,10 +43,14 @@ class PIDA_Widget extends \WP_Widget {
 		 * @param WP_Widget_Text $this        Current Text widget instance.
 		 */
 		$text = apply_filters( 'widget_text', $widget_text, $instance, $this );
+		$pida_highlight = $instance['pida_highlight'];
+		$pida_text = $instance['pida_text'];
+		$pida_background = $instance['pida_background'];
+
 
 		echo $args['before_widget'];
 		?>
-		<div class="textwidget"><?php echo !empty( $instance['filter'] ) ? wpautop( $text ) : $text; ?></div>
+		<div class="textwidget pida-widget" style="background-color: <?php echo $pida_background; ?>; color: <?php echo $pida_text; ?>; border-left: 3px solid <?php echo $pida_highlight; ?>;"><?php echo !empty( $instance['filter'] ) ? wpautop( $text ) : $text; ?></div>
 		<?php
 		echo $args['after_widget'];
 	}
@@ -56,13 +61,57 @@ class PIDA_Widget extends \WP_Widget {
 	 * @param array $instance The widget options
 	 */
 	public function form( $instance ) {
+
+		$pida_highlight = esc_attr($instance['pida_highlight']);
+		$pida_text = esc_attr($instance['pida_text']);
+		$pida_background = esc_attr($instance['pida_background']);
+		?>
+		<script type="text/javascript">
+			//<![CDATA[
+			jQuery(document).ready(function()
+			{
+				// colorpicker field
+				jQuery('.cw-color-picker').each(function(){
+					var $this = jQuery(this),
+						id = $this.attr('rel');
+
+					$this.farbtastic('#' + id);
+				});
+			});
+			//]]>
+		</script>
+
+		<?php
 		$instance = wp_parse_args( (array) $instance, array( 'text' => '' ) );
 		$filter = isset( $instance['filter'] ) ? $instance['filter'] : 0;
 		?>
+		<!--PIDA Text Box-->
 		<p><label for="<?php echo $this->get_field_id( 'text' ); ?>"><?php _e( 'Content:' ); ?></label>
 			<textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>"><?php echo esc_textarea( $instance['text'] ); ?></textarea></p>
 
 		<p><input id="<?php echo $this->get_field_id('filter'); ?>" name="<?php echo $this->get_field_name('filter'); ?>" type="checkbox"<?php checked( $filter ); ?> />&nbsp;<label for="<?php echo $this->get_field_id('filter'); ?>"><?php _e('Automatically add paragraphs'); ?></label></p>
+
+		<!--Highlight Color Picker-->
+		<p>
+			<label for="<?php echo $this->get_field_id('pida_highlight'); ?>"><?php _e('Highlight Color:'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('pida_highlight'); ?>" name="<?php echo $this->get_field_name('pida_highlight'); ?>" type="text" value="<?php if($pida_highlight) { echo $pida_highlight; } else { echo '#D2403F'; } ?>" />
+		<div class="cw-color-picker" rel="<?php echo $this->get_field_id('pida_highlight'); ?>"></div>
+		</p>
+		<!--Text Color Picker-->
+		<p>
+			<label for="<?php echo $this->get_field_id('pida_text'); ?>"><?php _e('Text Color:'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('pida_text'); ?>" name="<?php echo $this->get_field_name('pida_text'); ?>" type="text" value="<?php if($pida_text) { echo $pida_text; } else { echo '#000'; } ?>" />
+		<div class="cw-color-picker" rel="<?php echo $this->get_field_id('pida_text'); ?>"></div>
+		</p>
+		<!--Background Color Picker-->
+		<p>
+			<label for="<?php echo $this->get_field_id('pida_background'); ?>"><?php _e('Background Color:'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('pida_background'); ?>" name="<?php echo $this->get_field_name('pida_background'); ?>" type="text" value="<?php if($pida_background) { echo $pida_background; } else { echo '#ddd'; } ?>" />
+		<div class="cw-color-picker" rel="<?php echo $this->get_field_id('pida_background'); ?>"></div>
+		</p>
+
+
+
 		<?php
 	}
 
@@ -74,12 +123,10 @@ class PIDA_Widget extends \WP_Widget {
 	 */
 	public function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-		if ( current_user_can( 'unfiltered_html' ) ) {
-			$instance['text'] = $new_instance['text'];
-		} else {
-			$instance['text'] = wp_kses_post( $new_instance['text'] );
-		}
-		$instance['filter'] = ! empty( $new_instance['filter'] );
+		$instance['text'] = strip_tags($new_instance['text']);
+		$instance['pida_highlight'] = strip_tags($new_instance['pida_highlight']);
+		$instance['pida_text'] = strip_tags($new_instance['pida_text']);
+		$instance['pida_tbackground'] = strip_tags($new_instance['pida_background']);
 		return $instance;
 	}
 } //end PIDA Widget Class
