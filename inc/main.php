662c2f91c5c1921286271bad0e3c3c5938ff08dd
Nate Finch <finchproservices@gmail.com>
2016-08-25 16:12:28 +0900

    added categories, displays, need to fix the category input field so that it loads the current slugs on save/update

--- inc/main.php
+++ inc/main.php
@@ -27,9 +27,10 @@ class PIDA_Widget extends \WP_Widget {
 
 		/** This filter is documented in wp-includes/widgets/class-wp-widget-pages.php */
 
-		$widget_text = ! empty( $instance['text'] ) ? $instance['text'] : '';
-		$pida_highlight = ! empty( $instance['pida_highlight'] ) ? $instance['highlight'] : '';
 		$pida_text = ! empty( $instance['pida_text'] ) ? $instance['pida_text'] : '';
+		$pida_categories = ! empty( $instance['pida_categories'] ) ? $instance['pida_categories'] : '';
+		$pida_highlight = ! empty( $instance['pida_highlight'] ) ? $instance['pida_highlight'] : '';
+		$pida_text_color = ! empty( $instance['pida_text'] ) ? $instance['pida_text'] : '';
 		$pida_background = ! empty( $instance['pida_background'] ) ? $instance['pida_background'] : '';
 
 		/**
@@ -42,19 +43,20 @@ class PIDA_Widget extends \WP_Widget {
 		 * @param array          $instance    Array of settings for the current widget.
 		 * @param WP_Widget_Text $this        Current Text widget instance.
 		 */
-		$text = apply_filters( 'widget_text', $widget_text, $instance, $this );
+		$pida_text = apply_filters( 'pida_text', $pida_text, $instance, $this );
+		$pida_categories = $instance['pida_categories'];
 		$pida_highlight = $instance['pida_highlight'];
-		$pida_text = $instance['pida_text'];
+		$pida_text_color = $instance['pida_text_color'];
 		$pida_background = $instance['pida_background'];
 
 		//Set defaults if variables are empty
 		( $pida_highlight == '' ) ? $pida_highlight = '#D2403F' : null;
-		( $pida_text == '' ) ? $pida_text = '#000' : null;
+		( $pida_text_color == '' ) ? $pida_text_color = '#000' : null;
 		( $pida_background == '' ) ? $pida_background = '#ddd' : null;
 
 		echo $args['before_widget'];
 		?>
-		<div class="textwidget pida-widget" style="padding: 10px 10px 10px 20px; margin-bottom: 20px; background-color: <?php echo $pida_background; ?>; color: <?php echo $pida_text; ?>; border-left: 3px solid <?php echo $pida_highlight; ?>;"><?php echo !empty( $instance['filter'] ) ? wpautop( $text ) : $text; ?></div>
+		<div class="textwidget pida-widget" style="padding: 10px 10px 10px 20px; margin-bottom: 20px; background-color: <?php echo $pida_background; ?>; color: <?php echo $pida_text_color; ?>; border-left: 3px solid <?php echo $pida_highlight; ?>;"><?php echo !empty( $instance['filter'] ) ? wpautop( $pida_text ) : $pida_text; ?></div>
 		<?php
 		echo $args['after_widget'];
 
@@ -68,8 +70,9 @@ class PIDA_Widget extends \WP_Widget {
 	 */
 	public function form( $instance ) {
 
+		$pida_categories = esc_attr($instance['pida_categories']);
 		$pida_highlight = esc_attr($instance['pida_highlight']);
-		$pida_text = esc_attr($instance['pida_text']);
+		$pida_text_color = esc_attr($instance['pida_text_color']);
 		$pida_background = esc_attr($instance['pida_background']);
 		?>
 		<script type="text/javascript">
@@ -92,10 +95,15 @@ class PIDA_Widget extends \WP_Widget {
 		$filter = isset( $instance['filter'] ) ? $instance['filter'] : 0;
 		?>
 		<!--PIDA Text Box-->
-		<p><label for="<?php echo $this->get_field_id( 'text' ); ?>"><?php _e( 'Content:' ); ?></label>
-			<textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>"><?php echo esc_textarea( $instance['text'] ); ?></textarea></p>
+		<p><label for="<?php echo $this->get_field_id( 'pida_text' ); ?>"><?php _e( 'Content:' ); ?></label>
+			<textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id('pida_text'); ?>" name="<?php echo $this->get_field_name('pida_text'); ?>"><?php echo esc_textarea( $instance['pida_text'] ); ?></textarea></p>
 
+		<!--Add Paragraphs-->
 		<p><input id="<?php echo $this->get_field_id('filter'); ?>" name="<?php echo $this->get_field_name('filter'); ?>" type="checkbox"<?php checked( $filter ); ?> />&nbsp;<label for="<?php echo $this->get_field_id('filter'); ?>"><?php _e('Automatically add paragraphs'); ?></label></p>
+
+		<!--PIDA Category Input-->
+		<p><label for="<?php echo $this->get_field_id( 'pida_categories' ); ?>"><?php _e( 'What category or categories of posts do you want to display this announcement in? Comma separate this list and be sure to <b><u>use the category slugs</u></b>, e.g. "blog, food-recipes, personal".' ); ?></label>
+			<input class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id('pida_categories'); ?>" name="<?php echo $this->get_field_name('pida_categories'); ?>" placeholder="<?php echo _e('Current categories: ') . esc_textarea( $instance['pida_categories'] ); ?>"></p>
 
 		<!--Highlight Color Picker-->
 		<p>
@@ -105,9 +113,9 @@ class PIDA_Widget extends \WP_Widget {
 		</p>
 		<!--Text Color Picker-->
 		<p>
-			<label for="<?php echo $this->get_field_id('pida_text'); ?>"><?php _e('Text Color:'); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id('pida_text'); ?>" name="<?php echo $this->get_field_name('pida_text'); ?>" type="text" value="<?php if($pida_text) { echo $pida_text; } else { echo '#000'; } ?>" />
-		<div class="cw-color-picker" rel="<?php echo $this->get_field_id('pida_text'); ?>"></div>
+			<label for="<?php echo $this->get_field_id('pida_text_color'); ?>"><?php _e('Text Color:'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('pida_text_color'); ?>" name="<?php echo $this->get_field_name('pida_text_color'); ?>" type="text" value="<?php if($pida_text_color) { echo $pida_text_color; } else { echo '#000'; } ?>" />
+		<div class="cw-color-picker" rel="<?php echo $this->get_field_id('pida_text_color'); ?>"></div>
 		</p>
 		<!--Background Color Picker-->
 		<p>
@@ -129,9 +137,10 @@ class PIDA_Widget extends \WP_Widget {
 	 */
 	public function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-		$instance['text'] = strip_tags($new_instance['text']);
-		$instance['pida_highlight'] = strip_tags($new_instance['pida_highlight']);
 		$instance['pida_text'] = strip_tags($new_instance['pida_text']);
+		$instance['pida_categories'] = strip_tags($new_instance['pida_categories']);
+		$instance['pida_highlight'] = strip_tags($new_instance['pida_highlight']);
+		$instance['pida_text_color'] = strip_tags($new_instance['pida_text_color']);
 		$instance['pida_background'] = strip_tags($new_instance['pida_background']);
 		return $instance;
 	}
